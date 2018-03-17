28848e15bf03e4151d12382297ffa303584ad1c4
Peter Kassenaar <info@kassenaar.com>
2018-03-17 22:05:53 +0100

    Updates, fixed small typos. Removed testing etc.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,71 +3,70 @@ import {environment} from '../environments/environment';
 
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html'
 })
 export class AppComponent {
-  amount: number;
-  vat = 'inclusive';
-  tariff = environment.vatHigh;
+    amount: number;
+    vat = 'inclusive'; // TODO: make this a constant instead of string 'inclusive' or 'exclusive'
+    tariff = environment.vatHigh;
 
-  result: number;   // result including/excluding VAT
-  resultVat: number;  // amount of VAT
+    result: number;   // result including/excluding VAT
+    resultVat: number;  // amount of VAT
 
-  vatHigh: number = environment.vatHigh;
-  vatLow: number = environment.vatLow;
-  vatLow2019: number = environment.vatLow2019;
+    vatHigh: number = environment.vatHigh;
+    vatLow: number = environment.vatLow;
+    vatLow2019: number = environment.vatLow2019;
 
-  calculate() {
-    // Inclusive or Exclusive?
-    switch (this.vat) {
-      case 'inclusive': {
-        switch (this.tariff.toString()) {
-          case this.vatHigh.toString(): {
-            this.result = (this.amount / (1 + (this.vatHigh / 100)));
-            this.resultVat = this.amount - this.result;
-            break;
-          }
-          case this.vatLow.toString(): {
-            this.result = (this.amount / (1 + (this.vatLow / 100)));
-            this.resultVat = this.amount - this.result;
-            break;
-          }
-          case this.vatLow2019.toString(): {
-            this.result = ((this.amount * this.vatLow2019) / 100) + this.amount;
-            this.resultVat = this.amount - this.result;
-            break;
-          }
-          default:
-            break;
+    calculate() {
+        // Inclusive or Exclusive?
+        switch (this.vat) {
+            case 'inclusive': {
+                switch (this.tariff.toString()) {
+                    case this.vatHigh.toString(): {
+                        this.result = (this.amount / (1 + (this.vatHigh / 100)));
+                        this.resultVat = this.amount - this.result;
+                        break;
+                    }
+                    case this.vatLow.toString(): {
+                        this.result = (this.amount / (1 + (this.vatLow / 100)));
+                        this.resultVat = this.amount - this.result;
+                        break;
+                    }
+                    case this.vatLow2019.toString(): {
+                        this.result = ((this.amount * this.vatLow2019) / 100) + this.amount;
+                        this.resultVat = this.amount - this.result;
+                        break;
+                    }
+                    default:
+                        break;
+                }
+                break;
+            }
+            case 'exclusive': {
+                switch (this.tariff.toString()) {
+                    case this.vatHigh.toString(): {
+                        this.result = ((this.amount * this.vatHigh) / 100) + this.amount;
+                        this.resultVat = this.result - this.amount;
+                        break;
+                    }
+                    case this.vatLow.toString(): {
+                        this.result = ((this.amount * this.vatLow) / 100) + this.amount;
+                        this.resultVat = this.result - this.amount;
+                        break;
+                    }
+                    case this.vatLow2019.toString(): {
+                        this.result = ((this.amount * this.vatLow2019) / 100) + this.amount;
+                        this.resultVat = this.result - this.amount;
+                        break;
+                    }
+                    default:
+                        break;
+                }
+                break;
+            }
+            default:
+                break;
         }
-        break;
-      }
-      case 'exclusive': {
-        switch (this.tariff.toString()) {
-          case this.vatHigh.toString(): {
-            this.result = ((this.amount * this.vatHigh) / 100) + this.amount;
-            this.resultVat = this.result - this.amount;
-            break;
-          }
-          case this.vatLow.toString(): {
-            this.result = ((this.amount * this.vatLow) / 100) + this.amount;
-            this.resultVat = this.result - this.amount;
-            break;
-          }
-          case this.vatLow2019.toString(): {
-            this.result = ((this.amount * this.vatLow2019) / 100) + this.amount;
-            this.resultVat = this.result - this.amount;
-            break;
-          }
-          default:
-            break;
-        }
-        break;
-      }
-      default:
-        break;
-    }
-  }// end calculate()
+    }// end calculate()
 }
